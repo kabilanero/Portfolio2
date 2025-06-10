1371e1fd7701de8d34cf6c9e09f067dd9473ab0d
kabilanero <kapilrhode0000@gmail.com>
2025-06-10 21:42:29 +0530

    comited

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig( {
   // Base path for GitHub Pages
-  base:'/',
+  base:'/Portfolio2/',
   plugins: [react()],
   build: {
     outDir: 'dist',
